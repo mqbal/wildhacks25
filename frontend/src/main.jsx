7e4e06e74f9a8cdc0decd6d24530d20b767cd26e
HibatulM <hmuqe2@uic.edu>
2025-04-05 22:07:40 -0500

    changed main.jsx to get code from index.jsx instead of App.jsx and added the landing page code to the index.jsx file

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+// import App from './App.jsx'
 import App from './App.jsx'
+import Index from './index.jsx'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <Index />
   </StrictMode>,
 )
